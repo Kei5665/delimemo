ce299d74e8f21a6938fbc87b1fb41a784f219d8a
Kei <kt10195665@yahoo.ne.jp>
2022-06-30 15:50:16 +0900

    Add: 場所検索機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,15 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+    render turbo_stream: turbo_stream.replace(
+      'post_show',
+      partial: 'shared/error_messages',
+      locals: { object: @post },
+    )
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy!
