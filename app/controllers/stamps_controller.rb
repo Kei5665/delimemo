5ea09cd2ea90a34c2361765335a4841aa6c9726c
Kei <kt10195665@yahoo.ne.jp>
2022-07-08 19:26:03 +0900

    Add: 50メートル以内には良いているかどうかでスタンプ完了ボタンを出し分け完了ボタンを出し分ける

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -2,6 +2,8 @@ class StampsController < ApplicationController
   def create
     post = Post.find(params[:id])
     post.stamped = true
+
+    
     post.save!
     redirect_to root_path, success: "スタンプを完了しました！"
   end
