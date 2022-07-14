85274f81b7b86b7109680fb21a1f6f67fc34a610
Kei <kt10195665@yahoo.ne.jp>
2022-07-15 07:55:25 +0900

    Add: postとuserを紐づける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_120040) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_220122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_120040) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "stamped", default: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_120040) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
