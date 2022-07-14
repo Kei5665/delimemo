23f5bcee74c99e12ceef479f64f9f575b829463e
Kei <kt10195665@yahoo.ne.jp>
2022-07-14 21:20:00 +0900

    Add: ログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post "oauth/callback", to: "oauths#callback"
   get "oauth/callback", to: "oauths#callback"
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
+  get 'log_out', to: 'user_sessions#destroy', as: 'log_out'
   
   resources :posts, only: %i[create show destroy]
   resources :stamps, only: %i[create]
