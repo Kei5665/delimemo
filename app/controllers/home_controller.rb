0c782f88989b8fd964c6f03d871f8f796cd407c7
Kei <kt10195665@yahoo.ne.jp>
2022-07-19 15:57:51 +0900

    Fix: homeをpostに統一

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @post = Post.new
-
-    # スタンプを押してない投稿だけ取り出す
-    @posts = Post&.where(stamped: false)
-    gon.json = @posts.to_json
-  end
-end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
+  def index
+    @post = Post.new
+
+    # スタンプを押してない投稿だけ取り出す
+    @posts = Post&.where(stamped: false)
+    gon.json = @posts.to_json
+  end
+  
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
